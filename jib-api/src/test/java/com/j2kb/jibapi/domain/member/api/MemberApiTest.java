74e05da2b3feb34188887a576159c0610d2fc9ec
mkhwang <orange2652@gmail.com>
2021-05-01 02:51:31 +0900

    refactor : rename MemberControllerTest > MemberApiTest

--- jib-api/src/test/java/com/j2kb/jibapi/domain/member/api/MemberApiTest.java
+++ jib-api/src/test/java/com/j2kb/jibapi/domain/member/api/MemberApiTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @AutoConfigureMockMvc
 @SpringBootTest
-class MemberControllerTest {
+class MemberApiTest {
 
     @Autowired
     private ObjectMapper objectMapper;
