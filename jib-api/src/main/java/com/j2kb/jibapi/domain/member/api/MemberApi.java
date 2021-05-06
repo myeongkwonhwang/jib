c21265cc6c9f904fb09a61501df9b7b05a2fece0
mkhwang <orange2652@gmail.com>
2021-05-06 22:42:49 +0900

    style : add description

--- jib-api/src/main/java/com/j2kb/jibapi/domain/member/api/MemberApi.java
+++ jib-api/src/main/java/com/j2kb/jibapi/domain/member/api/MemberApi.java
@@ -3,6 +3,7 @@ package com.j2kb.jibapi.domain.member.api;
 import com.j2kb.jibapi.domain.member.dto.MemberJoinDto;
 import com.j2kb.jibapi.domain.member.service.MemberJoinService;
 import com.j2kb.jibapi.global.common.SuccessResponse;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -20,9 +21,21 @@ import javax.validation.Valid;
 @AllArgsConstructor
 public class MemberApi {
 
-    private MemberJoinService memberJoinService;
+    //Constructor Injection
+    private final MemberJoinService memberJoinService;
+
+    /* @AllArgsConstructor
+    public MemberApi(MemberJoinService memberJoinService) {
+        this.memberJoinService = memberJoinService;
+    }
+    사용하는 이유 field Injection
+    @Autowired 선언 아래 3개든 10개든 막 추가할 수 있다.
+    여기서 Constructor Injection을 사용하면 다른 Injection 타입에 비해 위기감 같은 걸 느끼게 해준다.
+    Constructor의 파라미터가 많아짐과 동시에 하나의 클래스가 많은 책임을 떠안는다는 걸 알게된다.
+    */
 
     @PostMapping
+    @ApiOperation(value = "기본회원가입")
     public SuccessResponse save(@Valid @RequestBody MemberJoinDto.BasicJoinReq req) {
         MemberJoinDto.BasicJoinRes response = memberJoinService.save(req);
         return SuccessResponse.success(response);
