0c3f9dfaaaba3c0bb84718465e09137dc678b4a5
mkhwang <orange2652@gmail.com>
2021-05-19 04:38:47 +0900

    fix : convert base64 to byte[]

--- jib-api/src/main/java/com/j2kb/jibapi/domain/user/service/UserJoinService.java
+++ jib-api/src/main/java/com/j2kb/jibapi/domain/user/service/UserJoinService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
+import java.nio.charset.StandardCharsets;
 
 @Service
 @RequiredArgsConstructor
@@ -23,8 +24,9 @@ public class UserJoinService extends BasicServiceSupport {
 
     public JoinDto.BasicRes create(@RequestBody @Valid JoinDto.BasicReq userReq) {
         controlParams(userReq);
-        User user = userRepository.save(modelMapper.map(userReq, User.class));
-        return modelMapper.map(user, JoinDto.BasicRes.class);
+        User user = modelMapper.map(userReq, User.class);
+        user.setValidationImg(userReq.getValidationImg().getBytes(StandardCharsets.UTF_8));
+        return modelMapper.map(userRepository.save(user), JoinDto.BasicRes.class);
     }
 
     private void controlParams(JoinDto.BasicReq userReq) {
