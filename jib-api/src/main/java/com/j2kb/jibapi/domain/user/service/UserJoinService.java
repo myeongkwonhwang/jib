62c0b2de4bf44834cfdc2e64a8bf285c6cebe498
문다훈 <ekgns00421@gmail.com>
2021-05-20 13:42:41 +0900

    feat : add user delete service & test codes

--- jib-api/src/main/java/com/j2kb/jibapi/domain/user/service/UserJoinService.java
+++ jib-api/src/main/java/com/j2kb/jibapi/domain/user/service/UserJoinService.java
@@ -4,13 +4,18 @@ import com.j2kb.jibapi.domain.interfaces.BasicServiceSupport;
 import com.j2kb.jibapi.domain.user.dao.UserRepository;
 import com.j2kb.jibapi.domain.user.dto.JoinDto;
 import com.j2kb.jibapi.domain.user.entity.User;
+import com.j2kb.jibapi.global.common.SuccessResponse;
+import com.j2kb.jibapi.global.error.exception.ErrorCode;
+import com.j2kb.jibapi.global.error.exception.InvalidValueException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +34,18 @@ public class UserJoinService extends BasicServiceSupport {
 
     private void controlParams(JoinDto.BasicReq userReq) {
         userReq.setPassword(bCryptPasswordEncoder.encode(userReq.getPassword()));
+    }
+    public User getUserByUserno(Integer userno) {
+        Optional<User> user = userRepository.findById(userno);
+        if(user.isEmpty()) throw new InvalidValueException(ErrorCode.ENTITY_NOT_FOUND);
+        return user.get();
+    }
+    public SuccessResponse delete(Integer userno){
+        User user = getUserByUserno(userno);
+        user.setState(0);
+        System.out.println(user.getState());
+        System.out.println(user.getEmail());
+        return new SuccessResponse(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase());
     }
 
 }
--- jib-api/src/test/java/com/j2kb/jibapi/domain/user/api/UserApiTest.java
+++ jib-api/src/test/java/com/j2kb/jibapi/domain/user/api/UserApiTest.java
@@ -6,6 +6,9 @@ import com.j2kb.jibapi.domain.user.entity.User;
 import com.j2kb.jibapi.domain.user.enums.LoginType;
 import com.j2kb.jibapi.domain.user.enums.UserType;
 import com.j2kb.jibapi.domain.user.service.UserJoinService;
+import org.hamcrest.core.Is;
+import org.hibernate.mapping.Join;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,15 +20,22 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -83,6 +93,7 @@ class UserApiTest {
             .usertype(UserType.STUDENT)
             .validationImg("img").build();
 
+
         JoinDto.BasicRes res = JoinDto.BasicRes.builder()
                 .email("hayeon@gmail.com")
                 .firstname("hayeon")
@@ -94,6 +105,7 @@ class UserApiTest {
         given(userJoinService.create(any())).willReturn(res);
 
         String json = objectMapper.writeValueAsString(req);
+        System.out.println(json);
 
         // when
         final ResultActions actions = mvc.perform(post("/api/v1/user")
@@ -107,5 +119,55 @@ class UserApiTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("hayeon")));
     }
+
+    @Test
+    @DisplayName("기본회원삭제")
+    public void deleteUser_test() throws Exception{
+
+        User user = new User();
+        user.setUserNo(1111);
+        user.setState(1);
+        user.setEmail("dahoon@gmail.com");
+
+//        JoinDto.BasicReq req = JoinDto.BasicReq.builder()
+//                .email("dahoon@gmail.com")
+//                .password("12341234")
+//                .firstname("dahoon")
+//                .lastname("moon")
+//                .logintype(LoginType.BASIC)
+//                .usertype(UserType.STUDENT)
+//                .validationImg("img")
+//                .state(1).build();
+//
+//        JoinDto.BasicRes res = JoinDto.BasicRes.builder()
+//                .email("hayeon@gmail.com")
+//                .firstname("hayeon")
+//                .lastname("kim")
+//                .logintype(LoginType.BASIC)
+//                .usertype(UserType.STUDENT)
+//                .build();
+//
+//        given(userJoinService.create(any())).willReturn(res);
+//        mvc.perform(post("/api/v1/user"))
+//        when(userJoinService).thenReturn(userJoinService.delete(user.getUserNo()));
+       final ResultActions resultActions =  mvc.perform(MockMvcRequestBuilders
+               .delete("/api/v1/user/{id}", 1111)
+               .contentType(MediaType.APPLICATION_JSON)
+               .characterEncoding("utf-8"));
+
+       resultActions.andDo(print())
+               .andExpect(status().isOk());
+
+
+
+
+
+
+//        given(userJoinService.delete(any())).willReturn();
+//
+//        final ResultActions res = mvc.perform(post("/api/v1/user").content());
+
+    }
+
 
 }
