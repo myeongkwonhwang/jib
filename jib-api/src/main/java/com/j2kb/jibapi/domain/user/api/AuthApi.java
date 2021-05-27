c07efc41fda6aed42ee824f6deb935a94086f997
Hayeon Kim <hayeon17kim@gmail.com>
2021-05-27 20:44:53 +0900

    add : AuthApi

---
+++ jib-api/src/main/java/com/j2kb/jibapi/domain/user/api/AuthApi.java
@@ -0,0 +1,47 @@
+package com.j2kb.jibapi.domain.user.api;
+
+import com.j2kb.jibapi.domain.jwt.JwtFilter;
+import com.j2kb.jibapi.domain.jwt.TokenProvider;
+import com.j2kb.jibapi.domain.user.dto.LoginDto;
+import com.j2kb.jibapi.domain.user.dto.TokenDto;
+import com.j2kb.jibapi.domain.user.entity.User;
+import com.j2kb.jibapi.global.config.security.UserDetailService;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthApi {
+
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailService userDetailService;
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto.BasicReq basicReq) {
+
+        String email = basicReq.getEmail();
+        String password = basicReq.getPassword();
+
+        final User user = userDetailService.findByEmailAndPassword(email, password);
+
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(email, password));
+
+        String jwt = tokenProvider.createToken(user);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
+
+    }
+}
