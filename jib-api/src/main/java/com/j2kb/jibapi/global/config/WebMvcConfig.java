5f98478bc1736b0a1eea1e7da801c068a665d07f
mkhwang <orange2652@gmail.com>
2021-05-29 16:57:28 +0900

    fix : add allowOrigins

--- jib-api/src/main/java/com/j2kb/jibapi/global/config/WebMvcConfig.java
+++ jib-api/src/main/java/com/j2kb/jibapi/global/config/WebMvcConfig.java
@@ -26,7 +26,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")	//TODO : 도메인 추가
+                .allowedOrigins("http://localhost:3000")//TODO : 도메인 추가
                 .allowedMethods(HttpMethod.GET.name()
                         , HttpMethod.PATCH.name()
                         , HttpMethod.PUT.name()
