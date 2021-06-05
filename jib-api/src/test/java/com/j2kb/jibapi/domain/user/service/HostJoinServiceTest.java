7ceffda7e6373cc4fd49222c8e85f8754e7340d2
mkhwang <orange2652@gmail.com>
2021-06-06 03:49:37 +0900

    test : Host회원가 테스트

---
+++ jib-api/src/test/java/com/j2kb/jibapi/domain/user/service/HostJoinServiceTest.java
@@ -0,0 +1,62 @@
+package com.j2kb.jibapi.domain.user.service;
+
+import com.j2kb.jibapi.domain.user.dto.JoinDto;
+import com.j2kb.jibapi.domain.user.enums.LoginType;
+import com.j2kb.jibapi.domain.user.enums.UserType;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by mkhwang on 2021/06/06
+ * Email : orange2652@gmail.com
+ * Github : https://github.com/myeongkwonhwang
+ */
+@SpringBootTest
+@Transactional
+@Slf4j
+class HostJoinServiceTest {
+
+    @Autowired
+    HostJoinService hostJoinService;
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    BigInteger getUserSequence() {
+        return (BigInteger) entityManager.createNativeQuery("select currval('jib.user_user_no_seq')").getSingleResult();
+    }
+
+    @Test
+    void hostCreateTest() {
+
+        JoinDto.HostBasicReq req = JoinDto.HostBasicReq.builder()
+                .email("orange2653@gmail.com")
+                .password("tg0850")
+                .firstName("myeongkwon")
+                .lastName("hwang")
+                .birthYear("1990")
+                .loginType(LoginType.BASIC)
+                .userType(UserType.HOST)
+                .build();
+
+        JoinDto.HostBasicRes hostBasicRes = hostJoinService.create(req);
+        Long userSequence = this.getUserSequence().longValue();
+        assertAll(
+                () -> assertEquals(hostBasicRes.getUserNo(), userSequence),
+                () -> assertEquals(hostBasicRes.getEmail(), "orange2653@gmail.com"),
+                () -> assertNotNull(hostBasicRes.getToken())
+        );
+
+    }
+
+}
