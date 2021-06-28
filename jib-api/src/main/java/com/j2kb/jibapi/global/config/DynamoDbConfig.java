f8220925358c29e78a7d9185d6dd74373a3080ef
문다훈 <ekgns00421@gmail.com>
2021-06-29 01:08:06 +0900

    feat : add host

--- jib-api/src/main/java/com/j2kb/jibapi/global/config/DynamoDbConfig.java
+++ jib-api/src/main/java/com/j2kb/jibapi/global/config/DynamoDbConfig.java
@@ -1,15 +1,23 @@
 package com.j2kb.jibapi.global.config;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.TimeZone;
 
 @Configuration
 @EnableDynamoDBRepositories
@@ -26,25 +34,38 @@ public class DynamoDbConfig {
     @Value("${amazon.aws.secretkey}")
     private String amazonDynamoDbSecretKey;
 
+    public static class LocalDateTimeConverter implements DynamoDBTypeConverter<Date, LocalDateTime>{
+        @Override
+        public Date convert(LocalDateTime src){
+            return Date.from(src.toInstant(ZoneOffset.UTC));
+        }
+        @Override
+        public LocalDateTime unconvert(Date src){
+            return src.toInstant().atZone(TimeZone.getDefault().toZoneId()).toLocalDateTime();
+        }
+    }
+
+
     @Bean
     public DynamoDBMapper dynamoDbMapper(){
         return new DynamoDBMapper(buildAmazonDynamoDB());
     }
-    private AmazonDynamoDB buildAmazonDynamoDB(){
+
+    @Bean
+    public AmazonDynamoDB buildAmazonDynamoDB(){
         return AmazonDynamoDBClientBuilder.standard()
-                .withEndpointConfiguration(
-                        new AwsClientBuilder.EndpointConfiguration(
-                                amazonDynamoDbEndPoint, "ap-northeast-2"
-                        )
-                )
-                .withCredentials(
-                        new AWSStaticCredentialsProvider(
-                                new BasicAWSCredentials(amazonDynamoDbAccessKey, amazonDynamoDbSecretKey)
-                        )
-                )
+                .withEndpointConfiguration(amazonEndpointConfiguration())
+                .withCredentials(awsCredentialsProvider())
                 .build();
     }
 
+    private AwsClientBuilder.EndpointConfiguration amazonEndpointConfiguration(){
+        return new AwsClientBuilder.EndpointConfiguration(amazonDynamoDbEndPoint, "ap-northeast-2");
+    }
 
+    @Bean
+    public AWSCredentialsProvider awsCredentialsProvider(){
+        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(amazonDynamoDbAccessKey, amazonDynamoDbSecretKey));
+    }
 
 }
