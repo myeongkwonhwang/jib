7df7d3101fa5f29ed24005488fc96aa4960283dd
Hayeon Kim <hayeon17kim@gmail.com>
2021-07-02 00:46:04 +0900

    fix : redis connect

--- jib-api/src/main/java/com/j2kb/JibApiApplication.java
+++ jib-api/src/main/java/com/j2kb/JibApiApplication.java
@@ -3,10 +3,12 @@ package com.j2kb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EntityScan("com.j2kb.jibapi.*") //JPA Entity Scan
+@EnableRedisRepositories(basePackages = "com.j2kb.jibapi.domain.user.dao")
 public class JibApiApplication {
 
     public static void main(String[] args) {
