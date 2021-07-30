9b0ab441051f3ba02404fef44267ac182f819ee1
Hayeon Kim <hayeon17kim@gmail.com>
2021-07-31 03:27:20 +0900

    update : paging test

--- jib-api/src/test/java/com/j2kb/jibapi/domain/destination/service/DestinationServiceTest.java
+++ jib-api/src/test/java/com/j2kb/jibapi/domain/destination/service/DestinationServiceTest.java
@@ -2,6 +2,7 @@ package com.j2kb.jibapi.domain.destination.service;
 
 import com.j2kb.jibapi.domain.destination.dto.DestinationDto;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,11 +28,18 @@ class DestinationServiceTest {
     @Test
     void searchDestinationByCountryTest() {
         String country = "대한민국";
-        List<DestinationDto.DestinationRes> destinationResList = destinationService.findDestinationByCountryOrderByNameAsc(country);
+        List<DestinationDto.DestinationRes> destinationResList = destinationService.findDestinationByCountryOrderByNameAsc(country, 1L, 20);
 
         for (DestinationDto.DestinationRes res : destinationResList) {
             assertEquals(res.getCountry(), country);
         }
+    }
+
+    @Test
+    void searchDestinationByCountryPagingTest() {
+        String country = "Republic of Korea";
+        List<DestinationDto.DestinationRes> destinationResList = destinationService.findDestinationByCountryOrderByNameAsc(country, 1L, 3);
+        Assertions.assertThat(destinationResList.size()).isEqualTo(3);
     }
 
 }
